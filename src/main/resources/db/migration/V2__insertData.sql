53d6e982e809d96a5aac019794c6154c9b5810ce
ext_mfortes <ext_mfortes@interbanking.com.ar>
2022-12-13 15:45:39 -0300

    w2m - Se agrego Flyway para insertar datos en la base al inciar

---
+++ src/main/resources/db/migration/V2__insertData.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO heroe(name) VALUES('SuperMan');
+INSERT INTO heroe(name) VALUES('SpiderMan');
+INSERT INTO heroe(name) VALUES('Hombre de Hierro');
+INSERT INTO heroe(name) VALUES('Capitan America');
